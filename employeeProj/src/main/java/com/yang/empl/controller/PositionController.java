4644151b5163e7e4f312650df796e557913a933c
tjdwlrwk3123 <tjdwlrwk3123@naver.com>
2021-06-08 19:06:19 +0900

    직위, 지역 삭제부분 제약조건 오류 세분화

--- employeeProj/src/main/java/com/yang/empl/controller/PositionController.java
+++ employeeProj/src/main/java/com/yang/empl/controller/PositionController.java
@@ -3,6 +3,7 @@ package com.yang.empl.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -43,9 +44,13 @@ public class PositionController {
 			pService.ppDelete(ppNum);
 			ra.addFlashAttribute("delete", "success");
 			return "redirect:position";
+		}catch(DataIntegrityViolationException de) {
+			System.out.println("제약조건 오류발생");
+			ra.addFlashAttribute("delete", "failed");
+			return "redirect:position";
 		}catch(Exception e) {
 			e.printStackTrace();
-			ra.addFlashAttribute("delete", "failed");
+			ra.addFlashAttribute("delete", "idk");
 			return "redirect:position";
 		}
 	}
--- employeeProj/src/main/java/com/yang/empl/controller/RegionController.java
+++ employeeProj/src/main/java/com/yang/empl/controller/RegionController.java
@@ -3,6 +3,7 @@ package com.yang.empl.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -45,9 +46,13 @@ public class RegionController {
 			rService.regionDelete(regionNum);
 			ra.addFlashAttribute("delete", "success");
 			return "redirect:region";
+		}catch(DataIntegrityViolationException de) {
+			System.out.println("제약조건 오류발생");
+			ra.addFlashAttribute("delete", "failed");
+			return "redirect:region";
 		}catch(Exception e) {
 			e.printStackTrace();
-			ra.addFlashAttribute("delete", "failed");
+			ra.addFlashAttribute("delete", "idk");
 			return "redirect:region";
 		}
 	}
